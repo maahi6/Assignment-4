a749a0915292f555262a247db9a48e2b04899786
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-11-25 07:37:29 -0800

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"calculator.h"
+using namespace std;
+int main()
+{
+	//Arithmetic operations with namespace cal
+	
+	cout << "Addition": << cal::sum(10,20)<< std::endl;
+	
+	cout << "Subtraction": << cal::sub(50,20)<< std::endl;
+
+	cout << "Product": << cal::mul(10,9)<< std::endl;
+
+	cout << "Division": << cal::div(50,5)<< std::endl;
+	
+	return 0;
+}
+
+
+
+
---
+++ calculator.h
@@ -0,0 +1,42 @@
+namespace cal
+{
+
+int add(int x , int y)
+{
+	return x + y;
+}
+int sub(int x , int y)
+{
+	return x - y;
+}
+int mul(int x , int y)
+{
+	return x * y;
+}
+int div(int x , int y)
+{
+    return x / y;
+}
+}
+/*
+namespace add{
+int add(int x , int y){
+	return x + y;
+    }
+}
+namespace sub{
+int sub(int x , int y){
+	return x - y;
+    }
+}
+namespace mul{
+int mul(int x , int y){
+	return x * y;
+    }
+}
+namespace div{
+int div(int x , int y){
+	return x/y;
+    }
+}
+*/
